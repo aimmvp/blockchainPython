164642969a89a2a426f6d1502de40ab802cea9df
s0wnd <aimmvp@gmail.com>
2018-05-25 16:18:17 +0900

    Step 1:Building a blockchain

---
+++ blockchain.py
@@ -0,0 +1,78 @@
+import hashlib
+import json
+from time import time
+from uuid import uuid4
+
+class Blockchain(object):
+    def _init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        # Creates a new Block and adds it to the chain
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time().,
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        # Adds a new transaction to the list of transactions
+        """
+        Creates a new transaction to go into the next mined Block
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        # pass
+        return self.last_block['index'] + 1
+    
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        # pass
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        # pass
+        """
+        Creates a SHA-256 hash of a Block
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
